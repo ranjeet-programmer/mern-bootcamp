042e61ec6e63518b95ee8fcca197f4e07c77af52
revenge_ff <62922876+ranjeet-programmer@users.noreply.github.com>
2021-07-19 15:43:32 +0000

    data-validation-added

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var router = express.Router();
+const { check, validationResult } = require("express-validator");
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name", "Name Should be At least 3 character").isLength({ min: 3 }),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password should be at least 3 character").isLength({
+      min: 3,
+    }),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
