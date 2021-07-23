86dfef6dfdea304d28bc575c9c9aa36ba1a63dd8
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2021-07-23 04:03:10 -0700

    Middlewared Added

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
   const errors = validationResult(req);
@@ -25,8 +27,75 @@ exports.signup = (req, res) => {
   });
 };
 
-exports.signout = (req, res) => {
-  res.json({
-    message: "User SignOut",
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "USER EMAIL DOES NOT EXISTS",
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and Password invalid",
+      });
+    }
+
+    // Create Token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+    // Put token in cookie
+
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    // send response to front end
+
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
   });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "User SignOut Successfully",
+  });
+};
+
+// PROTECTED ROUTES
+
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: " auth",
+});
+
+// CUSTOM MIDDLEWARES
+
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "you are not admin, access denied",
+    });
+  }
+  next();
 };
